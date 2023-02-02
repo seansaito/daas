f7400a810ae15c33e67bb18cd6c0672ce72f61c9
Saito-Sean <saito.sean@bcg.com>
2023-02-02 16:37:02 +0900

    Update

---
+++ backend/app/alembic/versions/bfa7457b7186_update.py
@@ -0,0 +1,24 @@
+"""Update
+
+Revision ID: bfa7457b7186
+Revises: ee5c421935ee
+Create Date: 2023-02-02 16:36:43.987745
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bfa7457b7186'
+down_revision = 'ee5c421935ee'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
